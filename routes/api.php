40d7e4b78d11ca0d866c8c18d79fa8ea2ce1e21f
vincent exelcio <vincent.celcio@gmail.com>
2024-11-22 00:21:16 +0700

    feat: add auth endpoints

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,20 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ProductController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Auth routes
+Route::post('/register', [AuthController::class, 'register']);
 
-Route::apiResource('/products', App\Http\Controllers\Api\ProductController::class);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::apiResource('/products', ProductController::class);
+
+// Protected routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
